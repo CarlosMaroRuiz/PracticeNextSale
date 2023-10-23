cfa346cf01e96dc2e423bb7e4437299229889afd
Carlos Mario Ruiz Pinacho <105567234+CarlosMario123@users.noreply.github.com>
2023-10-23 09:33:45 -0600

    carrito agregado

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,19 +5,43 @@ import ListProducts from "./ListProducts";
 export default function Cart() {
     const[ver,setVer] = useState(false);
     const{state} = contextGlobal();
+    const [conteo,setConteo] = useState(0);
+    const [mensaje,setMensaje] = useState("");
    
       useEffect(()=>{
+          
+        if(state.length == 0){
+         setMensaje("0")
+        }else{
+          
+          let count = state.reduce((accumulator, product) => {
+            return accumulator + product.cantidad;
+          }, 0); // 0 es el valor inicial del acumulador
+          
+          console.log(`La cantidad total de productos es: ${count}`);
+          
+          
+          setConteo(count);
+          
+          if(count >= 9){
+            setMensaje("+9")
+          }else{
+            setMensaje(conteo)
+          }
+        }
+            
           return () => {
           };
     
-      },[state])
-
+      },[state,conteo])
+      
+      //Este use efect nos permitira contar los elementos 
       const visualizar = ()=>{
           setVer(!ver);
       }
   return (
     <>
-      <div className="absolute flex items-center justify-center p-1 bg-green-300 rounded-full w-7 h-7 right-1 top-1">{state.length}</div>
+      <div className="absolute flex items-center justify-center p-1 bg-green-300 rounded-full w-7 h-7 right-1 top-1">{mensaje}</div>
       <button className="p-2 text-white rounded-sm bg-sky-400" onClick={visualizar}>Carrito</button>
        {(ver) && <ListProducts/>}
     </>
